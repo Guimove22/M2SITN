4001366f41c04cb51ee4d3ca6936c90de1779a91
Axel <axel.richier@gmail.com>
2014-10-10 11:05:48 +0200

    Exercice de mind mapping ajouté

---
+++ Management_des_connaissances/MindMapping.mm
@@ -0,0 +1,101 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Num&#xe9;risation de&#xa;l&apos;Espace de Bataille (NEB)" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1412927951766"><hook NAME="MapStyle" zoom="1.728">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="6"/>
+<font BOLD="false"/>
+<node TEXT="&#xc9;changes" POSITION="right" ID="ID_850873859" CREATED="1412925037228" MODIFIED="1412927737042" HGAP="28" VSHIFT="-12">
+<edge COLOR="#ff0000"/>
+<node TEXT="Ordres" ID="ID_1633564740" CREATED="1412925659446" MODIFIED="1412926233291" VSHIFT="-20">
+<node TEXT="Ordre d&apos;Op&#xe9;ration" ID="ID_331757093" CREATED="1412926239173" MODIFIED="1412926246603"/>
+<node TEXT="Ordre Pr&#xe9;paratoire" ID="ID_779456530" CREATED="1412926247152" MODIFIED="1412926258591"/>
+</node>
+<node TEXT="Retour Sur Ordres" ID="ID_864726011" CREATED="1412925680325" MODIFIED="1412928329572" VSHIFT="20"/>
+</node>
+<node TEXT="Hi&#xe9;rarchie" POSITION="left" ID="ID_56620352" CREATED="1412925221595" MODIFIED="1412927539936" HGAP="11" VSHIFT="-39">
+<edge COLOR="#0000ff"/>
+<node TEXT="Brigade/Grande Unit&#xe9;" ID="ID_1893910180" CREATED="1412925235441" MODIFIED="1412926931947" HGAP="40" VSHIFT="-30"/>
+<node TEXT="R&#xe9;giment" ID="ID_1617553034" CREATED="1412925271703" MODIFIED="1412927069908" HGAP="60">
+<node TEXT="Commandant R&#xe9;giment" ID="ID_1861445097" CREATED="1412926509403" MODIFIED="1412926609838" HGAP="40" VSHIFT="-20"/>
+</node>
+<node TEXT="Compagnie" ID="ID_647958499" CREATED="1412925608504" MODIFIED="1412927074535" HGAP="40" VSHIFT="40">
+<node TEXT="Commandant d&apos;Unit&#xe9;" ID="ID_1114741135" CREATED="1412926620074" MODIFIED="1412927074535" HGAP="50" VSHIFT="20"/>
+</node>
+</node>
+<node TEXT="Fonctionnement G&#xe9;n&#xe9;ral" POSITION="right" ID="ID_550015639" CREATED="1412925368588" MODIFIED="1412927940335" HGAP="25" VSHIFT="-18">
+<edge COLOR="#ff00ff"/>
+<node TEXT="Proc&#xe9;dure Sans SN" ID="ID_1200304567" CREATED="1412925378459" MODIFIED="1412927938861" HGAP="31" VSHIFT="-2">
+<icon BUILTIN="stop"/>
+<node TEXT="" ID="ID_1209202395" CREATED="1412925473611" MODIFIED="1412925473611"/>
+</node>
+<node TEXT="Proc&#xe9;dure Avec SN (solution initiale)" ID="ID_1971745492" CREATED="1412925480715" MODIFIED="1412927943431" HGAP="17" VSHIFT="24">
+<icon BUILTIN="prepare"/>
+<node TEXT="Proc&#xe9;dure SN avec Backbrief" ID="ID_44408640" CREATED="1412925561874" MODIFIED="1412927943430" HGAP="-167" VSHIFT="50">
+<icon BUILTIN="go"/>
+</node>
+</node>
+</node>
+<node TEXT="&#xc9;laboration de la solution" POSITION="left" ID="ID_471171257" CREATED="1412926381077" MODIFIED="1412927760084" HGAP="10">
+<edge COLOR="#00ffff"/>
+<node TEXT="&#xc9;mission d&apos;ordre direct" ID="ID_1625459339" CREATED="1412927228848" MODIFIED="1412928243790">
+<node TEXT="M&#xc9;DO" ID="ID_1407598846" CREATED="1412927248203" MODIFIED="1412927302967"/>
+<node TEXT="R&#xe9;daction" ID="ID_1441421742" CREATED="1412927305667" MODIFIED="1412927312336"/>
+<node TEXT="V&#xe9;rification" ID="ID_3162971" CREATED="1412928271686" MODIFIED="1412928278986"/>
+<node TEXT="Transmission" ID="ID_1255553320" CREATED="1412927317468" MODIFIED="1412927321523"/>
+</node>
+<node TEXT="Backbrief" ID="ID_405786907" CREATED="1412927272805" MODIFIED="1412927565963" HGAP="47" VSHIFT="24">
+<node TEXT="R&#xe9;ception" ID="ID_1863586120" CREATED="1412927379725" MODIFIED="1412927384016"/>
+<node TEXT="Analyse" ID="ID_1345835464" CREATED="1412927384930" MODIFIED="1412927387431"/>
+<node TEXT="Retour" ID="ID_788114268" CREATED="1412927388058" MODIFIED="1412927390510"/>
+<node TEXT="Validation" ID="ID_1063187206" CREATED="1412927391172" MODIFIED="1412927565962"/>
+</node>
+</node>
+</node>
+</map>
