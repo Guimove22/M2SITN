394f80464bdb4546f24d7d32c1a45ef929f677fe
Axel <axel.richier@gmail.com>
2014-11-03 10:11:03 +0100

    management et banque // Uml agile)

---
+++ management_et_banque/ITIL/ITIL in a nutshell   ZDNet_files/init1.js
@@ -0,0 +1,9 @@
+//<script>
+
+bouncex.state = {"iol":true,"tn":1414975147,"rc":false,"mobile":false,"RECORD_CAMPAIGN_EVENT_ENABLED":1,"EV":""};
+bouncex.cookie = {"fvt":1414975147,"vid":1414975147185203,"ao":0,"as":0,"vpv":1,"d":"d","lp":"http%3A%2F%2Fwww.zdnet.com%2Fblog%2Fgovernment%2Fitil-in-a-nutshell%2F1842","r":"https%253A%252F%252Fwww.google.fr%252F","cvt":1414975147,"gcr":28,"m":0,"lvt":1414975147};
+bouncex.campaigns = false;
+bouncex.debug = false;
+bouncex.website.bau = 'http://bounceexchange.com/';
+
+bouncex.init();
